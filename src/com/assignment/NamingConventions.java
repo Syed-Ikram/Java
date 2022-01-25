4b1660f9b0e08c79f36abaac648b74b392d09b63
Ikram <syed.khaja.ikramuddin9@gmail.com>
2022-01-25 14:11:22 +0530

    changed NamingConventions

--- src/com/assignment/NamingConventions.java
+++ src/com/assignment/NamingConventions.java
@@ -1,5 +1,21 @@
 package com.assignment;
-
+/*
+Names rules
+1.Choose descriptive and unambiguous names.
+2.Make meaningful distinction.
+3.Use pronounceable names.
+4.Use searchable names.
+5.Replace magic numbers with named constants.
+6.Avoid encodings. Don't append prefixes or type information.
+Functions rules
+1.Small.
+2.Do one thing.
+3.Use descriptive names.
+4.Prefer fewer arguments.
+5.Have no side effects.
+6.Don't use flag arguments.
+  Split method into several independent methods that can be called from the client without the flag.
+ */
 import java.time.*;
 class DateAndTime{
     LocalDateTime present;
